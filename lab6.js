14feeb9d7c5bbcb30195c5e89e2b8bd7e45963b8
David Seu 916 <david.seu@stud.ubbcluj.ro>
2024-04-02 11:44:45 +0300

    lab6 done

---
+++ lab6.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#openModal').click(function() {
+        $("#modal").show();
+        // $("#overlay").show();
+    });
+
+    // $('#closeModal').click(function() {
+    //     // var values = $('#modal input').map(function() {
+    //     //     return $(this).val();
+    //     // }).get().join(' ');
+
+    //     // $('#result').val(values);
+    //     $('#modal').hide();
+    //     $('#overlay').hide();
+    //     // $('#mainForm input').prop('disabled', false);
+    // });
+});
